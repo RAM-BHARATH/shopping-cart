fa5782ea0d69b76f340d7f28c6049d8d005e0579
RAM-BHARATH <ramtherapid007@gmail.com>
2022-02-11 19:45:58 +0530

    Added styling for shop.js

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,14 +1,13 @@
 import Item from "./Item";
-import { Link } from 'react-router-dom';
 import ShoppingCart from "./ShoppingCart";
 import NavBar from "./NavBar";
+import '../styles/Shop.css'
 
 const Shop = ({ items, addToCart, cartItems, findCartItemById, updateCount, deleteCartItem, emptyCart }) =>{
 
     return(
         <div className="Shop">
             <NavBar />
-            <p>Shop</p>
             <div className="items">
                 {items.map(item=>(
                     <Item key={item.itemId} item={item} id={item.itemId} addToCart={addToCart} cartItems={cartItems} findCartItemById={findCartItemById} updateCount={updateCount}/>
