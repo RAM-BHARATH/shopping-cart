d694632527437713e760b7f77dc936b226a7044f
RAM-BHARATH <ramtherapid007@gmail.com>
2022-02-14 22:25:01 +0530

    Updated home, Shop and Cart style

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,32 +1,39 @@
 import { Link } from "react-router-dom";
-
-const ShoppingCart = ({ cartItems, updateCount, deleteCartItem, emptyCart }) =>{
+import '../styles/ShoppingCart.css';
+const ShoppingCart = ({ cartItems, updateCount, deleteCartItem, emptyCart, totalAmount }) =>{
 
     // const [currItemCount, setCurrItemCount] = useState(findCartItemById(currItemId)?findCartItemById(currItemId).count:1);
 
     return(
-        <div>
+        <div className="Cart">
             <div>
                 <h1>Your cart</h1>
-                <button onClick={()=>(emptyCart())}>Empty cart</button>
+                <button onClick={()=>(emptyCart())} className="empty-cart">Empty cart</button>
+                <h2>Total Cost: ${totalAmount}</h2>
                 {/* <Link to="/shop">Go to Shop</Link>
                 <Link to="/">Go home</Link> */}
             </div>
             {cartItems.length>0?
-            <ol>
+            <div className="items">
                 {cartItems.map(cartItem=>{
                     return(
-                        <li key={cartItem.id}>
-                            {console.log("Shopping cart")}
-                            {console.log(cartItem)}
-                            <span>{cartItem.name},{cartItem.count}</span>
-                            <button onClick={()=>(updateCount(cartItem.id, cartItem.count+1))}>+</button> 
-                            <button onClick={()=>(updateCount(cartItem.id, cartItem.count-1))}>-</button>
-                            <button onClick={()=>(deleteCartItem(cartItem.id))}>Delete</button>
-                        </li>
+                        <div key={cartItem.id} className="item cart-item">
+                            {/* {console.log("Shopping cart")}
+                            {console.log(cartItem)} */}
+                            <img src={cartItem.cartItemImageLocation} alt="" />
+                            <div class="cart-item-info-container">
+                                <div>{cartItem.name}</div>
+                                <div className="cart-item-options">
+                                    <button onClick={()=>(updateCount(cartItem.id, cartItem.count-1))}>-</button>
+                                    <div>{cartItem.count}</div>
+                                    <button onClick={()=>(updateCount(cartItem.id, cartItem.count+1))}>+</button> 
+                                    <button onClick={()=>(deleteCartItem(cartItem.id))} className='delete'>Delete</button>
+                                </div>
+                            </div>
+                        </div>
                     )
                 })}
-            </ol>
+            </div>
             :'Your cart is empty'}
         </div>
         
