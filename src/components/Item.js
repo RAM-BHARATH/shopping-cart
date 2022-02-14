2c14ec14bcdc7c75fb58c35e5f2d5fb32a82ecd3
RAM-BHARATH <ramtherapid007@gmail.com>
2022-02-14 22:19:38 +0530

    Added hooks for image location, cost

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react/cjs/react.development";
+// import '../styles/I.css'
 
 const Item = ({ item, id, addToCart, cartItems, findCartItemById, updateCount }) =>{
     // const [currItem, setCurrItem] = useState(item);
     const [currName, setCurrName] = useState(item.name);
     const [currItemId, setCurrItemId] = useState(item.itemId);
     const [currItemCount, setCurrItemCount] = useState(findCartItemById(currItemId)?findCartItemById(currItemId).count:1);
-
+    const [currItemCost, setCurrItemCost] = useState(item.itemCost);
+    const [currImgLocation, setCurrImgLocation] = useState(item.imageLocation);
     
     useEffect(()=>{
         console.log("Current item count:",currItemCount)
@@ -32,14 +34,23 @@ const Item = ({ item, id, addToCart, cartItems, findCartItemById, updateCount })
             setCurrItemCount(currItemCount+1)
             await updateCount(currItemId, currItemCount+1);
         }else{
-            await addToCart({name:currName,id:currItemId, count:currItemCount})
+            await addToCart({name:currName,id:currItemId, count:currItemCount, cartItemCost: currItemCost, cartItemImageLocation: currImgLocation})
         }
     }
     return(
-        <div className="item" onClick={()=>onClickFunctions()} id={item.itemId}>
-            <p>{item.name}</p>
+        <div className="item" id={item.itemId}>
+            <img src={`${item.imageLocation}`} alt={`${item.name}`}/>
+            <div className="item-info">
+                <span>{item.name}</span>
+                <span>${item.itemCost}</span>
+            </div>
+            <div onClick={()=>onClickFunctions()} className="add-to-cart">Add to cart</div>
+            {/* <p>{item.name},{item.itemCost}</p> */}
+            
             {/* {console.log(findCartItemById(currItemId)?findCartItemById(currItemId).count:'')} */}
             {/* {console.log(currItem)} */}
+            {/* {console.log("Item.js")}
+            {console.log(item)} */}
         </div>
     );
 };
