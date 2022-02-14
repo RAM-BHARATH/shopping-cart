1f15aaac5ea7f43f63479287fa73e369db836637
RAM-BHARATH <ramtherapid007@gmail.com>
2022-02-14 22:18:52 +0530

    Added CartItemCount in Navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,13 @@
 import { Link } from 'react-router-dom'
 import '../styles/NavBar.css'
 
-const NavBar = () =>{
+const NavBar = ({ cartItemCount }) =>{
     return(
         <nav className="navbar">
             <ul>
                 <Link className="navlink" to="/">Home</Link>
                 <Link className="navlink" to="/shop">Shop</Link>
-                <Link className="navlink" to="/cart">Cart</Link>
+                <Link className="navlink" to="/cart">Cart{cartItemCount>0?`(${cartItemCount})`:''}</Link>
             </ul>
         </nav>
     )
